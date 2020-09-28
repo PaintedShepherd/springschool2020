15017aa542fab2b6204e0caf2766896dc833ee92
Malte Schäfer <malte.schaefer@fh-erfurt.de>
2020-09-28 15:51:59 +0200

    updated transformation sync

--- Assets/TransformSynchronizer.cs
+++ Assets/TransformSynchronizer.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 
 public class TransformSynchronizer : MonoBehaviourPun, IPunObservable
 {
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if(stream.IsWriting)
-        {
-            stream.SendNext(this.transform.position);
-        }
-        else
-        {
-            this.transform.position = (Vector3)stream.ReceiveNext();
-        }
-    }
+    private Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(this.transform.position);
+            stream.SendNext(this.transform.rotation);
+
+            if(rigidbody)
+            {
+                stream.SendNext(rigidbody.isKinematic);
+                stream.SendNext(rigidbody.useGravity);
+            }
+        }
+        else
+        {
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
+
+            if(rigidbody)
+            {
+                rigidbody.isKinematic = (bool)stream.ReceiveNext();
+                rigidbody.useGravity = (bool)stream.ReceiveNext();
+            }
+        }
     }
 }
