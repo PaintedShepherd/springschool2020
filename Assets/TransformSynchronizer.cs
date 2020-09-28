7038a9d61cc49e6c861822a4599a2037ae682ab9
Malte Schäfer <malte.schaefer@fh-erfurt.de>
2020-09-28 16:49:03 +0200

    update

--- Assets/TransformSynchronizer.cs
+++ Assets/TransformSynchronizer.cs
@@ -6,17 +6,27 @@ using UnityEngine;
 public class TransformSynchronizer : MonoBehaviourPun, IPunObservable
 {
     private Rigidbody rigidbody;
+    private Renderer renderer;
+    private string materialName;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+        materialName = renderer.material.name;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (materialName != renderer.material.name)
+        {
+            Material yourMaterial = (Material)Resources.Load(materialName, typeof(Material));
+
+            renderer.material = yourMaterial;
+
+        }
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -31,16 +41,32 @@ public class TransformSynchronizer : MonoBehaviourPun, IPunObservable
                 rigidbody.isKinematic = false;
                 rigidbody.useGravity = true;
             }
+
+            stream.SendNext(this.tag);
+
+            if (renderer)
+            {
+                materialName = renderer.material.name;
+                stream.SendNext(materialName);
+            }
+
         }
         else
         {
             this.transform.position = (Vector3)stream.ReceiveNext();
             this.transform.rotation = (Quaternion)stream.ReceiveNext();
 
-            if(rigidbody)
+            if (rigidbody)
             {
                 rigidbody.isKinematic = true;
                 rigidbody.useGravity = false;
+            }
+
+            this.tag = (string)stream.ReceiveNext();
+
+            if (renderer)
+            {
+                materialName = (string)stream.ReceiveNext();
             }
         }
     }
