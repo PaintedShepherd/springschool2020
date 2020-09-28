f62c2ea478c372e52022800a0302d64e868d5f59
Malte Schäfer <malte.schaefer@fh-erfurt.de>
2020-09-28 15:15:48 +0200

    added synchronizer script

---
+++ Assets/TransformSynchronizer.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSynchronizer : MonoBehaviourPun, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(this.transform.position);
+        }
+        else
+        {
+            this.transform.position = (Vector3)stream.ReceiveNext();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
