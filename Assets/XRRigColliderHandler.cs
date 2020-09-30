774f2ee8d83153f77f8b5e88e74c72f01bd526b1
Malte Schäfer <malte.schaefer@fh-erfurt.de>
2020-09-30 13:20:24 +0200

    fixed teleporter for player

---
+++ Assets/XRRigColliderHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRRigColliderHandler : MonoBehaviour
+{
+    public BoxCollider boxCollider;
+    private GameObject head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        head = GetComponent<XRRig>().cameraGameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newCenter = head.transform.localPosition;
+
+        boxCollider.center = newCenter;
+    }
+}
